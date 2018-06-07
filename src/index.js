dd0a6bbd6e0e1d51e90274391dd478a298f60279
n0psledbyte <n0psledbyte@gmail.com>
2018-06-07 09:52:42 +0700

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+//import './index.css';
+//import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './redux-wallet/css/itemslist.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './redux-wallet/components/App';
+import store from './redux-wallet/stores/store';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
